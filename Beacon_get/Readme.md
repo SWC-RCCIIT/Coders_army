154bd5697b479462f6aef212ae8fc8629d4f68e3
joardar-aditya <youngjoardar@gmail.com>
2020-01-21 20:42:59 +0530

    room database app added

--- Beacon_get/Readme.md
+++ Beacon_get/Readme.md
@@ -2,3 +2,4 @@
 
 Find the broadcaster app here-: <a href="https://drive.google.com/file/d/1KI9UVW_6rLYnu888F7eoemn0CzLE-4wh/view?usp=sharing">
  App for download </a>
+
---
+++ Room_database_adder/app/src/main/java/com/hummbletech/room_database_adder/add_room.java
@@ -0,0 +1,16 @@
+package com.hummbletech.room_database_adder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class add_room extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_room);
+
+
+    }
+}
